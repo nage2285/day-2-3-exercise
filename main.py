1bf170eb033d4873971077aa6f1b5fb804f6b5eb
cliffordcoco <88732726+nage2285@users.noreply.github.com>
2021-08-10 16:35:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+#print(type(age))
+time_left = int(age)
+#print(time_left)
+R_days = (90 - time_left)*365
+R_weeks = (90 - time_left)*52
+R_months = (90 - time_left)*12
+print(f"You have {R_days} days, {R_weeks} weeks, and {R_months} months left")
+
+
+
+
+
+
+
